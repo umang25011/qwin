c9af0a5abfa8c065e383d00aaa9ed9490661a908
Umang Patel <umang25011@gmail.com>
2023-07-27 21:06:08 -0400

    added new landing page

--- src/layouts/DemodayPage/DemodaypageComponant.tsx
+++ src/layouts/DemodayPage/DemodaypageComponant.tsx
@@ -1,64 +1,155 @@
-import React from 'react';
+import "./demoDayPage.css"
+import Photo1 from "./photo1.jpg"
+import Photo2 from "./photo2.jpg"
 
-const DemodaypageComponant: React.FC = () => {
+export default function DemodaypageComponant() {
   return (
-    <>
-      {/*header section start */}
-      <div className="header_section">
-        {/* ... (rest of the HTML content) ... */}
-      </div>
-      {/*header section end */}
-      {/*about section start */}
-      <div className="about_section layout_padding">
-        {/* ... (rest of the HTML content) ... */}
-      </div>
-      {/*about section end */}
-      {/*services section start */}
-      <div className="services_section layout_padding">
-        {/* ... (rest of the HTML content) ... */}
-      </div>
-      {/*services section end */}
-      {/*blog section start */}
-      <div className="blog_section layout_padding">
-        {/* ... (rest of the HTML content) ... */}
-      </div>
-      {/*blog section end */}
-      {/*newsletter section start */}
-      <div className="newsletter_section">
-        {/* ... (rest of the HTML content) ... */}
-      </div>
-      {/*newsletter section end */}
-      {/*client section start */}
-      <div className="client_section layout_padding">
-        {/* ... (rest of the HTML content) ... */}
-      </div>
-      {/*client section end */}
-      {/*footer section start */}
-      <div className="footer_section layout_padding">
-        {/* ... (rest of the HTML content) ... */}
-      </div>
-      {/*client section end */}
-      {/*javascript */}
-      <script src="js/jquery.min.js"></script>
-      <script src="js/popper.min.js"></script>
-      <script src="js/bootstrap.bundle.min.js"></script>
-      <script src="js/jquery-3.0.0.min.js"></script>
-      <script src="js/plugin.js"></script>
-      {/* sidebar */}
-      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-      <script src="js/custom.js"></script>
-      {/* javascript */}
-      <script src="js/owl.carousel.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
-      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.0.0-beta.2.4/owl.carousel.min.js"></script>
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js">')</script>
-      <script src="../../assets/js/vendor/popper.min.js"></script>
-      <script src="../../dist/js/bootstrap.min.js"></script>
-    </script>
-    </>
-  );
-  
-};
+    <body>
+      <header>
+        <div className="company-logo">Qwin</div>
+        <nav className="navbar1">
+          <ul className="nav-items">
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                HOME
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                OFFER
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                SHOP
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                CONTACT
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="menu-toggle">
+          <i className="bx bx-menu"></i>
+          <i className="bx bx-x"></i>
+        </div>
+      </header>
+      <main>
+        <section className="container section-1">
+          <div className="slogan">
+            <h1 className="company-title">UNIVERSITY OF WINDSOR </h1>
+            <h2 className="company-slogan">The biggest computer science event in the area.</h2>
+          </div>
+          <div className="home-computer-container">
+            <img className="home-computer home-img" src={Photo1} alt="a computer in dark with shadow" />
+          </div>
+        </section>
 
-export default DemodaypageComponant;
+        <section className="container section-2">
+          <div className="offer offer-1">
+            <img src={Photo2} alt="a computer in dark with with white shadow" className="offer-img offer-1-img" />
+            <div className="offer-description offer-desc-1">
+              <h2 className="offer-title">Macbook pro</h2>
+              <p className="offer-hook">This a Macbook pro nulla vulputate magna vel odio sagittis bibendium...</p>
+              <div className="cart-btn">ADD TO CART</div>
+            </div>
+          </div>
+          <div className="offer offer-2">
+            <img
+              src="https://github.com/r-e-d-ant/Computer-store-website/blob/main/assets/images/offer_2.png?raw=true"
+              alt="a opened computer"
+              className="offer-img offer-2-img"
+            />
+            <div className="offer-description offer-desc-2">
+              <h2 className="offer-title">Lenovo</h2>
+              <p className="offer-hook">This a Lenovo nulla vulputate magna vel odio sagittis bibendium...</p>
+              <div className="cart-btn">ADD TO CART</div>
+            </div>
+          </div>
+        </section>
+
+        <section className="container section-4">
+          <div className="sponsor sponsor-1">
+            <img
+              src="https://raw.githubusercontent.com/r-e-d-ant/Computer-store-website/b90ac65459206fef22e9b87313f587185554263b/assets/images/microsoft.svg"
+              alt=""
+            />
+          </div>
+          <div className="sponsor sponsor-2">
+            <img
+              src="https://raw.githubusercontent.com/r-e-d-ant/Computer-store-website/b90ac65459206fef22e9b87313f587185554263b/assets/images/apple.svg"
+              alt=""
+            />
+          </div>
+
+          <div className="sponsor sponsor-3">
+            <img
+              src="https://raw.githubusercontent.com/r-e-d-ant/Computer-store-website/b90ac65459206fef22e9b87313f587185554263b/assets/images/dell.svg"
+              alt=""
+            />
+          </div>
+          <div className="sponsor sponsor-4">
+            <img
+              src="https://raw.githubusercontent.com/r-e-d-ant/Computer-store-website/b90ac65459206fef22e9b87313f587185554263b/assets/images/ibm.svg"
+              alt=""
+            />
+          </div>
+        </section>
+
+        <section className="container section-5">
+          <h2 className="subscribe-input-label">Demo Day</h2>
+        </section>
+      </main>
+      <footer>
+        <div className="container top-footer">
+          <div className="footer-item">
+            <h2 className="footer-title">ADDRESS</h2>
+            <div className="footer-items">
+              <h3>300 Ouellette Ave</h3>
+            </div>
+          </div>
+          <div className="footer-item">
+            <h2 className="footer-title">SERVICES</h2>
+            <div className="footer-items">
+              <h3>Adipisicing elit.</h3>
+              <h3>Adipisicing elit.</h3>
+              <h3>Adipisicing elit.</h3>
+              <h3>Adipisicing elit.</h3>
+              <h3>Adipisicing elit.</h3>
+            </div>
+          </div>
+          <div className="footer-item">
+            <h2 className="footer-title">SUPPLIERS</h2>
+            <div className="footer-items">
+              <h3>Adipisicing elit.</h3>
+              <h3>Adipisicing elit.</h3>
+              <h3>Adipisicing elit.</h3>
+              <h3>Adipisicing elit.</h3>
+              <h3>Adipisicing elit.</h3>
+            </div>
+          </div>
+          <div className="footer-item">
+            <h2 className="footer-title">INVESTMENT</h2>
+            <div className="footer-items">
+              <h3>Adipisicing elit.</h3>
+              <h3>Adipisicing elit.</h3>
+              <h3>Adipisicing elit.</h3>
+              <h3>Adipisicing elit.</h3>
+              <h3>Adipisicing elit.</h3>
+            </div>
+          </div>
+        </div>
+        <div className="container end-footer">
+          <div className="copyright">
+            copyright © 2023 - Present • <b>Qwin</b>
+          </div>
+          <a className="designer" href="#">
+            University of Windsor
+          </a>
+        </div>
+      </footer>
+    </body>
+  )
+}
